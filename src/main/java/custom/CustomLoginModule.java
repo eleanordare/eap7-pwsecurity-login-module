d634ff87bbd04b4401b2d59c9705e5e999e6e4cf
root <eleanor@redhat.com>
2016-12-05 11:36:50 -0500

    first commit

---
+++ src/main/java/custom/CustomLoginModule.java
@@ -0,0 +1,76 @@
+package custom;
+
+import java.security.acl.Group;
+import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginException;
+
+import org.jboss.security.SimpleGroup;
+import org.jboss.security.SimplePrincipal;
+import org.jboss.security.auth.spi.UsernamePasswordLoginModule;
+
+public class CustomLoginModule extends UsernamePasswordLoginModule {
+
+	@Override
+	public boolean login(){
+		return PWSecurityImpl.login(username, password);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public void initialize(Subject subject, CallbackHandler callbackHandler,
+			Map sharedState, Map options) {
+
+		super.initialize(subject, callbackHandler, sharedState, options);
+	}
+
+	/**
+	 * Compares the result of this method with the entered password.
+	 * If validatePassword isn't overridden here, it will do a String compare.
+	 */
+	@Override
+	protected String getUsersPassword() throws LoginException {
+
+		System.out.println("Authenticating user " + getUsername());
+
+		String password = super.getUsername();
+		return password;
+	}
+
+	@Override
+	protected boolean validatePassword(String inputPassword,
+			String expectedPassword) {
+
+		return true;
+	}
+
+	/**
+	 * (required) The groups of the user. Must be at least one group called
+	 * "Roles" for user.
+	 */
+	@Override
+	protected Group[] getRoleSets() throws LoginException {
+
+		SimpleGroup group = new SimpleGroup("Roles");
+		
+		PWPermission[] permissions = getAllPermissions(getUsername(), getApplicationId());
+		for (PWPermission i : permissions) {
+			group.addMember(new SimplePrincipal(i));
+			
+		}
+		
+		try {
+			group.addMember(new SimplePrincipal("admin"));
+		} catch (Exception e) {
+			throw new LoginException("Failed to create group member for "
+					+ group);
+		}
+
+		System.out.println("Role for user " + getUsername() + ": "
+				+ group.members().nextElement().toString());
+
+		return new Group[] { group };
+	}
+
+}
